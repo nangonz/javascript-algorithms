d972ef41cb271a2b4dbd95e29938d2b6c4c93be5
nangonz <nangonz91@gmail.com>
2024-01-27 16:10:04 -0300

    divisible sum pairs challenge

--- adventJs.js
+++ adventJs.js
@@ -1208,3 +1208,38 @@ function concat(array, type){
   
   return array.join('').concat( type === 'M'? '()': '')
 }
+
+/* 
+  DIVISIBLE SUM PAIR
+  Given an array of integers and a positive integer K, determine
+  the number of (i,j) pairs where i<j and ar[i] + ar[j] is divisible  
+  by k  
+
+  EXAMPLE:  
+  ar = [ 1,2,3,4,5,6]  
+  k = 5  
+  Three pairs meet the criteria [1,4], [2,3] and [4,6]  
+
+  PARAMETERS:  
+  int n: the length of array ar  
+  int ar[n]: an array of integers  
+  int k: the integer divisor  
+
+  RETURNS  
+  int: the number of pairs  
+
+  INPUT FORMAT:  
+  The first line contains 2 space-separated integers, n and k.  
+  The second line contains n space-separated integers, each a value of ar[i]  
+
+  SAMPLE INPUT:
+  * STDIN
+  6 3 
+  1 3 2 6 1 2
+  * FUNCTION
+  n=6, k=3
+  [1, 3, 2, 2, 6, 1, 2]
+  
+  SAMPLE OUTPUT
+  5 -> Valid pairs
+*/
