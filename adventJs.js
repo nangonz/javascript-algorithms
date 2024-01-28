934ce55e855583fdfbc73c6d7bef03c5e53b0daa
nangonz <nangonz91@gmail.com>
2024-01-28 19:39:23 -0300

    gradingStudents challenge solved

--- adventJs.js
+++ adventJs.js
@@ -1340,3 +1340,83 @@ function plusMinus (arr) {
 }
 
 // console.log(plusMinus([-4, 3, -9, 0, 4, 1]))
+
+/*
+  LONELY INTEGER
+  Given an array of integers, where all elements but one occur twice  
+  find the unique element.
+
+  EXAMPLE:
+  a = [1,2,3,4,3,2,1]
+  The unique element is 4
+
+  FUNCTION DESCRIPTION:
+  Complete the lonely integer in the editor below.
+  lonely integer has the following parameters:
+  * int a[n]: an array of integers
+
+  RETURNS:
+  * int: the element that occurs only once
+
+  INPUT FORMAT:
+  The first line contains a single integer, n, the number of  
+  integers in the array. The second line contains line n space-separated  
+  integers that describe the values in a.
+*/
+
+function lonelyinteger (a) {
+  for (let int of a) {
+    if(a.indexOf(int) === a.lastIndexOf(int)) return int
+  }
+}
+
+// console.log(lonelyinteger([2,3,5,6,2,5,3]))
+
+/*
+  GRADING STUDENTS
+  HackerLand University has the following grading policy:  
+  Every student receives a grade in the inclusive range from 0 to 100.  
+  Any grade less than 40 is a failing grade. Sam is a professor at the university and likes  
+  to round each student's grade according to these rules:
+  
+  * If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+  * If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+  EXAMPLES:
+  * grade = 84 round to 85 (85-84 is less than 3)
+  * grade = 29 do not round (result is less than 40)
+  * grade = 57 do not round (60-57 is 3 or higher)
+  Given the initial value of grade for each of Sam's n students, write code to automate the rounding process.
+
+  FUNCTION DESCRIPTION:
+  Complete the function gradingStudents in the editor below.
+  gradingStudents has the following parameter(s):
+  int grades[n]: the grades before rounding.
+
+  RETURNS:
+  int[n]: the grades after rounding as appropriate.
+
+*/
+
+function nextMult(int) {
+  while (int % 5 !== 0) {
+    int ++
+  }
+  return int
+}
+
+function gradingStudents (grades) {
+  let roundedGrades = []
+  for (let grade of grades) {
+    let multiple = nextMult(grade)
+    let difference = multiple - grade
+    if (multiple >= 40 && difference < 3) {
+      roundedGrades.push(multiple)
+    } else {
+      roundedGrades.push(grade)
+    }
+  }
+    return roundedGrades
+}
+
+console.log(gradingStudents([33, 57, 84, 78, 29]))
