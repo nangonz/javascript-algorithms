9fcf3225168122b4a45f032659b6177bc8fdcdcc
nangonz <nangonz91@gmail.com>
2024-02-09 15:05:33 -0300

    missing numbers challenge statement

--- challenges.js
+++ challenges.js
@@ -2680,3 +2680,31 @@ function dynamicArrayAlt(n, queries) {
 }
 
 // console.log(dynamicArrayAlt(2, ["105", "117", "103", "210", "211"]))
+
+/*MISSING NUMBERS ------------------------------------------------------
+  Given two arrays of integers, find which elements in the second array are 
+  missing from the first array.
+
+  EXAMPLE:
+  arr = [7,2,5,3,5,3]
+  brr = [7,2,5,4,6,3,5,3]
+  The brr array is the original list. The numbers missing are [4,6]
+
+  NOTES: 
+    * If a number occurs multiple times in the list, you must ensure that 
+    the frequency of that number in both lists is the same. If that is not 
+    the case, then it is also a missing number.
+    * Return the missing numbers sorted ascending.
+    * Only include a missing number once, even if it is missing multiple times.
+    * The difference between the maximum an minimum numbers in the original
+      list is less tha or equal to 100.
+
+  FUNCTION DESCRIPTION:
+  It should return a sorted array of missing numbers. missingNumbers has the
+  following parameters:
+    * int arr[n]: the array with missing numbers.
+    * int brr[m]: the original array of numbers.
+  
+  RETURNS:
+    * int[]: an array of integers.
+*/
