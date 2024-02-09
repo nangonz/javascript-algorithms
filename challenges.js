b55b617a2293c213a2ddc3d9483ef070f0052617
nangonz <nangonz91@gmail.com>
2024-02-09 11:07:02 -0300

    Dynamic array challenge statement

--- challenges.js
+++ challenges.js
@@ -2603,3 +2603,25 @@ function minimumNumber (n, password) {
 }
 
 // console.log(minimumNumber(7, "AUzs-nV"))
+
+/* DYNAMIC ARRAY
+  * Declate a 2-dimensional array, arr, of n empty arrays. All arrays are
+    zero indexed.
+  * Declare an integer, "lastAnswer", and initialize it to ()
+  * There are two types of queries, given as an array of strings for you to
+    parse:
+
+    1. Query: 1 x y
+      1. Let idx = ((x ⊕ lastAnswer) % n)
+      2. Append the integer y to arr[idx]
+    
+    2. Query: 2 x y
+      1. Let idx = ((x ⊕ lastAnswer) % n)
+      2. Assign the value arr[idx][y % size(arr[idx])] to lastAnswer
+      3. Store the new value of lastAnswer to an answers array.
+
+  NOTE: ⊕ is the bitwise XOR operation which corresponds to the ^ operator
+  in most languages. % is the modulo operator. Finally, size(arr[idx]) is the
+  number of elements in arr[idx].
+
+*/
