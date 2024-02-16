b6964591b71865b5455817ac64cfafce41425574
nangonz <nangonz91@gmail.com>
2024-02-16 16:04:22 -0300

    Misere Nim challenge statement

--- challenges.js
+++ challenges.js
@@ -2895,3 +2895,22 @@ function balancedSums(arr) {
 console.log(balancedSums([1,1,4,1,1]))
 console.log(balancedSums([2,0,0,0]))
 console.log(balancedSums([0,0,2,0]))
+
+/*  MISERE NIM
+  Two people are playing game of Misere Nim. The basic rules for this game are
+  as follows:
+
+    * The game atarts with n piles of stones indexed from 0 to n-1. Eack pile
+      i (where 0 <= i < n) has s subínd i stones.
+    * The players move in alternating turns. During each move, the current player
+      must remove one or more stones from a single pile.
+    * The player who removes the last stone loses the game.
+
+  Given the value of n and the number of stones in each pile, determine whether the 
+  person who wins the game es the first or second person to move. If the first player
+  to move wins, print First on a new line; otherwise print Second. Assume both players
+  move optimally.
+
+  
+
+*/
