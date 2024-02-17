b3a0b21de5596515d5d4b8b944adc09f6106721d
nangonz <nangonz91@gmail.com>
2024-02-17 18:11:56 -0300

    forming a magic square challenge

--- challenges.js
+++ challenges.js
@@ -3049,4 +3049,19 @@ function isBalanced (string) {
   return stack.length == 0? "YES": "NO";
 }
 
-console.log(isBalanced("([{[n}}])"))
+// console.log(isBalanced("([{[n}}])"))
+
+/* FORMING A MAGIC SQUARE ---------------------------------------------------
+  We define a "magic square" to be an nxn matrix of distinct positive integers
+  from 1 to n2 where the sum of any row, column, or diagonal of length n is 
+  always equal to the same number: the magic constant.
+
+  You will be given a 3x3 matrix s of integers in the inclusive range [1,9]
+  We can convert any digit a to any other digit b in the range [1,9] at cost of
+  [a-b]. Given s, convert it into a magic square at minimal cost. Print this cost
+  on a new line.
+
+  NOTE: The resulting magic square must contain distinct integers in the inclusive
+  range [1,9]
+
+*/
