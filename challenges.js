327814bd83f3e1993d75c594b2d822af535094ff
nangonz <nangonz91@gmail.com>
2024-02-18 20:57:19 -0300

    superDigit function description and example

--- challenges.js
+++ challenges.js
@@ -3122,9 +3122,35 @@ console.log(formingMagicSquare([[5, 3, 4], [1, 5, 8], [6, 4, 2]]))
     * Otherwise, the super digit of x is equal to the super digit of
       the sum of the digits of x.
   For example, the super digit of 9875 will be calculated as:
+
   super_digit(9875)    9+8+7+5 = 29
   super_digit(29)      2+9 = 11
   super_digit(11)      1+1 = 2
   super_digit(2)       = 2
+
+  EXAMPLE:
+  n = ' 9875'
+  k = 4
+  The number p is created by concatening the string n k times so the initial
+  p = 9875987598759875
+
+  superDigit(p) = superDigit(9875987598759875)
+                  9+8+7+5+9+8+7+5+9+8+7+5+9+8+7+5 = 116
+  superDigit(p) = superDigit(116)
+                  1+1+6 = 8
+  superDigit(p) = superDigit(8)
+
+  All of the digits of p sum to 116. The digits of 116 sum to 8. 8 is only
+  one digit, so it is the super digit.
+
+  FUNCTION DESCRIPTION:
+  superDigit function must return the calculated super digit as an integer
+  superDigit has the following parameters:
+    * string n: a string representation of an integer.
+    * int k: the times to concatenate n to make p.
+  
+  RETURNS:
+    *  int: the super digit of n repeated k times.
+
 
 */
