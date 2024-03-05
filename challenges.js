88c2ec8372ce51f5b8428d02c32b7ed00a4160e0
nangonz <nangonz91@gmail.com>
2024-03-04 22:03:36 -0300

    sherlock valid string challenge

--- challenges.js
+++ challenges.js
@@ -3541,6 +3541,13 @@ function pylons(k, arr) {
 // console.log(pylons(3, [0,1,1,1,1,0])) 
 // console.log(pylons(3, [0,0,0,1,1,0]))
 
-/*
+/* SHERLOCK AND THE VALID STRING
+  Sherlock considers a string to be valid if all characters of the string appear
+  the same number of times. It is also valid if he can remove just 1 character
+  at 1 index in the string, and the remaining characters will occur the same
+  number of times. Given a string s determine if it is a valid string. Returs 
+  YES or NO.
+
+  
 
 */
