9e41628540677944cef680407e2c09b719a120b5
nangonz <nangonz91@gmail.com>
2024-03-06 15:44:52 -0300

    climbing the leaderboard challenge example

--- challenges.js
+++ challenges.js
@@ -3612,6 +3612,12 @@ console.log(isValid("abbcc"))
     * The player with the highest score is ranked number 1 on the leaderboard.
     * Players who have equal scores receive the same ranking number, and the 
       next player(s) receive the immediately following ranking number.
+  EXAMPLE:
+  ranked = [100, 90, 80]
+  player = [70, 80, 105]
+  The ranked players will have ranks 1, 2, 2, and 3 respectively. If the player's
+  scores are 70, 80 and 105 their rankings after each game are 4th, 3rd and 1st.
+  Return [4, 3, 1]
 
 
 */
