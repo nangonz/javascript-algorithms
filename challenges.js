5c36c6eabd9890eeb45d19f38765615af8a1d9da
nangonz <nangonz91@gmail.com>
2024-03-06 15:42:15 -0300

    climbing the leaderboard challenge statement

--- challenges.js
+++ challenges.js
@@ -3605,3 +3605,13 @@ console.log(isValid("abcdefghhgfedecba")) //NO
 console.log(isValid("aabbcd"))
 console.log(isValid("aabc"))
 console.log(isValid("abbcc"))
+
+/* CLIMBING THE LEADERBOARD
+  An arcade game player wants to climb to the top of the leaderboard and track 
+  their ranking. The game uses Dense Ranking, so its leaderboard works like this:
+    * The player with the highest score is ranked number 1 on the leaderboard.
+    * Players who have equal scores receive the same ranking number, and the 
+      next player(s) receive the immediately following ranking number.
+
+
+*/
