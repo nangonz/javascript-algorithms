e26bcaa2914510f9f28c9417a2fed912ca7a2b88
nangonz <nangonz91@gmail.com>
2024-07-08 00:17:11 -0300

    add insertionSort challenge resolution

--- challenges.js
+++ challenges.js
@@ -3877,4 +3877,22 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log(bubbleSort([1, 5, 4, 2, 8]));
+// console.log(bubbleSort([1, 5, 4, 2, 8]));
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let auxiliar = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > auxiliar) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = auxiliar;
+  }
+  return arr;
+}
+
+console.log(insertionSort([1, 5, 2, 4, 8, 3]));
+// 1, 2, 5, 4, 8, 3
+//       4  5
